671d0adec1ba25c966adeee579958bebf8e48d70
PedroScheffer <pscheffergoncalves@gmail.com>
2025-10-25 15:50:58 -0300

    Atualizações da Gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "devandroid.pedroscheffer.metronomohinario"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "devandroid.pedroscheffer.metronomohinario"
